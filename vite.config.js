44cb62deb016201a4004345783a5750153548f62
Leonid Lavrov <l.lavrov@kari.com>
2025-10-10 19:45:39 +0500

    fix: ci/cd

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   
   plugins: [react()],
+  base: '/vibro_compare',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     proxy: {
       '/api': {
